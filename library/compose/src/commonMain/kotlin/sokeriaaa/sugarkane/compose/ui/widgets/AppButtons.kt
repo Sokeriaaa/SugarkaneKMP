26f3eb19e17f020f9ac2dce09aae5d703eddc3e3
Sokeriaaa <73162265+Sokeriaaa@users.noreply.github.com>
2026-04-12 12:53:58 +0800

    Add AppButtons.kt

---
+++ library/compose/src/commonMain/kotlin/sokeriaaa/sugarkane/compose/ui/widgets/AppButtons.kt
@@ -0,0 +1,329 @@
+/**
+ * Copyright (C) 2026 Sokeriaaa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sokeriaaa.sugarkane.compose.ui.widgets
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import sokeriaaa.sugarkane.compose.res.Res
+import sokeriaaa.sugarkane.compose.res.back
+
+/**
+ * Simplified for default buttons to avoid nesting.
+ *
+ * @see Button
+ */
+@Composable
+fun AppButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    iconRes: DrawableResource? = null,
+    contentDescription: String? = null,
+    text: String,
+    shape: Shape = ButtonDefaults.shape,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource? = null,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource,
+    ) {
+        iconRes?.let {
+            Icon(
+                painter = painterResource(it),
+                contentDescription = contentDescription,
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+            )
+            Spacer(
+                modifier = Modifier.size(ButtonDefaults.IconSpacing),
+            )
+        }
+        Text(text)
+    }
+}
+
+/**
+ * Simplified for default buttons to avoid nesting.
+ *
+ * @see FilledTonalButton
+ */
+@Composable
+fun AppFilledTonalButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    iconRes: DrawableResource? = null,
+    contentDescription: String? = null,
+    text: String,
+    shape: Shape = ButtonDefaults.filledTonalShape,
+    colors: ButtonColors = ButtonDefaults.filledTonalButtonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.filledTonalButtonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    FilledTonalButton(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource,
+    ) {
+        iconRes?.let {
+            Icon(
+                painter = painterResource(it),
+                contentDescription = contentDescription,
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+            )
+            Spacer(
+                modifier = Modifier.size(ButtonDefaults.IconSpacing),
+            )
+        }
+        Text(text)
+    }
+}
+
+/**
+ * Simplified for default buttons to avoid nesting.
+ *
+ * @see OutlinedButton
+ */
+@Composable
+fun AppOutlinedButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    iconRes: DrawableResource? = null,
+    contentDescription: String? = null,
+    text: String,
+    shape: Shape = ButtonDefaults.outlinedShape,
+    colors: ButtonColors = ButtonDefaults.outlinedButtonColors(),
+    elevation: ButtonElevation? = null,
+    border: BorderStroke? = ButtonDefaults.outlinedButtonBorder(enabled),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource? = null,
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource,
+    ) {
+        iconRes?.let {
+            Icon(
+                painter = painterResource(it),
+                contentDescription = contentDescription,
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+            )
+            Spacer(
+                modifier = Modifier.size(ButtonDefaults.IconSpacing),
+            )
+        }
+        Text(text)
+    }
+}
+
+/**
+ * Simplified for default buttons to avoid nesting.
+ *
+ * @see TextButton
+ */
+@Composable
+fun AppTextButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.textShape,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+    elevation: ButtonElevation? = null,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
+    interactionSource: MutableInteractionSource? = null,
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource,
+    ) {
+        Text(text)
+    }
+}
+
+/**
+ * Simplified for default buttons to avoid nesting.
+ *
+ * @see IconButton
+ */
+@Composable
+fun AppIconButton(
+    modifier: Modifier = Modifier,
+    iconRes: DrawableResource,
+    onClick: () -> Unit,
+    contentDescription: String?,
+    enabled: Boolean = true,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        colors = colors,
+        interactionSource = interactionSource,
+    ) {
+        Icon(
+            painter = painterResource(iconRes),
+            contentDescription = contentDescription,
+        )
+    }
+}
+
+/**
+ * Simplified for default buttons to avoid nesting.
+ *
+ * @see FilledIconButton
+ */
+@Composable
+fun AppFilledIconButton(
+    modifier: Modifier = Modifier,
+    iconRes: DrawableResource,
+    onClick: () -> Unit,
+    contentDescription: String?,
+    enabled: Boolean = true,
+    shape: Shape = IconButtonDefaults.filledShape,
+    colors: IconButtonColors = IconButtonDefaults.filledIconButtonColors(),
+    interactionSource: MutableInteractionSource? = null,
+) {
+    FilledIconButton(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        interactionSource = interactionSource,
+    ) {
+        Icon(
+            painter = painterResource(iconRes),
+            contentDescription = contentDescription,
+        )
+    }
+}
+
+/**
+ * Simplified for default buttons to avoid nesting.
+ *
+ * @see FilledIconButton
+ */
+@Composable
+fun AppFilledTonalIconButton(
+    modifier: Modifier = Modifier,
+    iconRes: DrawableResource,
+    onClick: () -> Unit,
+    contentDescription: String?,
+    enabled: Boolean = true,
+    shape: Shape = IconButtonDefaults.filledShape,
+    colors: IconButtonColors = IconButtonDefaults.filledTonalIconButtonColors(),
+    interactionSource: MutableInteractionSource? = null,
+) {
+    FilledTonalIconButton(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        interactionSource = interactionSource,
+    ) {
+        Icon(
+            painter = painterResource(iconRes),
+            contentDescription = contentDescription,
+        )
+    }
+}
+
+/**
+ * Common back button for the TopBar, etc.
+ *
+ * @see AppIconButton
+ * @see IconButton
+ */
+@Composable
+fun AppBackIconButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) = AppIconButton(
+    modifier = modifier,
+    iconRes = backIconRes,
+    onClick = onClick,
+    contentDescription = stringResource(Res.string.back),
+    enabled = enabled,
+    colors = colors,
+    interactionSource = interactionSource,
+)
+
+internal expect val backIconRes: DrawableResource
